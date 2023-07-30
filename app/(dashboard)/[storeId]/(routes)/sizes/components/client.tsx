becb7b5c9b8158fd7018bce4b3fa17051c1faeaf
ceepu8 <caophuonguyen132002@gmail.com>
2023-07-30 15:24:34 +0700

    feat(size): build UI size pages

--- app/(dashboard)/[storeId]/(routes)/sizes/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/components/client.tsx
@@ -6,14 +6,14 @@ import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { Plus } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
-import { CategoryColumn, columns } from "./columns";
+import { SizeColumn, columns } from "./columns";
 import ApiList from "@/components/ui/api-list";
 
-interface CategoryClientProps {
-  data: CategoryColumn[];
+interface SizeClientProps {
+  data: SizeColumn[];
 }
 
-const CategoryClient: React.FC<CategoryClientProps> = ({ data }) => {
+const SizeClient: React.FC<SizeClientProps> = ({ data }) => {
   const params = useParams();
   const router = useRouter();
 
@@ -21,23 +21,22 @@ const CategoryClient: React.FC<CategoryClientProps> = ({ data }) => {
     <>
       <div className="flex items-center justify-between">
         <Heading
-          title={`Categories (${data.length})`}
-          description="Manage categories for your store"
+          title={`Sizes (${data.length})`}
+          description="Manage sizes for your store"
         />
-        <Button
-          onClick={() => router.push(`/${params.storeId}/categories/new`)}
-        >
+
+        <Button onClick={() => router.push(`/${params.storeId}/sizes/new`)}>
           <Plus className="mr-2 h-4 w-4" />
           Add new
         </Button>
       </div>
       <Separator />
       <DataTable searchKey="name" columns={columns} data={data} />
-      <Heading title="API" description="API calls for Categories" />
+      <Heading title="API" description="API calls for Sizes" />
       <Separator />
-      <ApiList entityName="categories" entityIdName="categoryId" />
+      <ApiList entityName="sizes" entityIdName="sizeId" />
     </>
   );
 };
 
-export default CategoryClient;
+export default SizeClient;
---
+++ app/(dashboard)/[storeId]/(routes)/sizes/new/page.tsx
@@ -0,0 +1,18 @@
+import prismadb from "@/lib/prismadb";
+import SizeForm from "../components/size-form";
+
+const BillboardPage = async ({
+  params,
+}: {
+  params: { billboardId: string };
+}) => {
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <SizeForm />
+      </div>
+    </div>
+  );
+};
+
+export default BillboardPage;
